74c79dfe94e16b9092cfefbad664b744d3031bf0
Berezh <verezh@gmail.com>
2023-06-09 01:22:08 +0300

    number

--- src/v1/zippers/number.ts
+++ src/v1/zippers/number.ts
@@ -1,13 +1,12 @@
+import { Number32 } from "../lib/number32";
 import { Zipper } from "./zipper";
 
 export class NumberZipper extends Zipper<number> {
   public zip(source: number): string {
-    const result = "";
-
-    return result;
+    return Number32.toBase32(source);
   }
 
   public unzip(zipped: string): number {
-    return parseFloat(zipped);
+    return Number32.toNumber(zipped);
   }
 }
